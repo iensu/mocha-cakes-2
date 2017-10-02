5bd2056b0d49e57158b0f30305489ec8401808fd
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-10-02 12:00:34 +0200

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "mocha-cakes-2" {
+    export function Feature(t: string, f: Function): any;
+    export function Scenario(t: string, f: Function): any;
+    export function Given(t: string, f: Function): any;
+    export function And(t: string, f: Function): any;
+    export function When(t: string, f: Function): any;
+    export function Then(t: string, f: Function): any;
+}
