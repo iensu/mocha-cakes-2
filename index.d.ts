798e51274b5eb265fa268b443c508c5d09dfe0ed
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-10-02 12:27:35 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,12 @@ declare function Given(t: string, f: Function): any;
 declare function And(t: string, f: Function): any;
 declare function When(t: string, f: Function): any;
 declare function Then(t: string, f: Function): any;
+declare function But(t: string, f: Function): any;
+
+declare function feature(t: string, f: Function): any;
+declare function scenario(t: string, f: Function): any;
+declare function given(t: string, f: Function): any;
+declare function and(t: string, f: Function): any;
+declare function when(t: string, f: Function): any;
+declare function then(t: string, f: Function): any;
+declare function but(t: string, f: Function): any;
